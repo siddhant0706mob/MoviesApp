3c8c7662bd0494bd418341d29bf05d8a40ab31d8
siddhant0706mob <2001siddhantranjan@gmail.com>
2025-07-07 20:45:57 +0530

    inital files

---
+++ MovieApp/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  MovieApp
+//
+//  Created by Siddhant Ranjan on 07/07/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+      
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Siddhant Ranjan on 07/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
